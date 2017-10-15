2e90fcab331a262a799b6e9a8539f6670315a063
mattyw <gh@mattyw.net>
2017-10-15 22:31:26 +0100

    command line args, not random yet

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,59 @@
 module Main where
 
 import Lib
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Opts = Opts
+    { words :: Int
+    , letters :: Int
+    , seperator :: String
+    , complexity :: Int
+    , ubuntu :: Bool
+    }
+
+options :: Parser Opts
+options = Opts
+    <$> option auto
+        ( long "words"
+        <> short 'w'
+        <> metavar "INT"
+        <> help "Number of words in the name"
+        <> showDefault
+        <> value 2 )
+    <*> option auto
+        ( long "letters"
+        <> short 'l'
+        <> metavar "INT"
+        <> help "Maximum number of letters in each word"
+        <> showDefault
+        <> value 0 )
+    <*> option auto
+        ( long "seperator"
+        <> short 's'
+        <> metavar "STR"
+        <> help "String used to seperate name words"
+        <> showDefault
+        <> value "-" )
+    <*> option auto
+        ( long "complexity"
+        <> short 'c'
+        <> metavar "INT"
+        <> help "0 = easy words, 1 = standard words, 2 = complex words"
+        <> showDefault
+        <> value 1 )
+    <*> switch
+        ( long "ubuntu"
+        <> short 'u'
+        <> help "generate ubuntu-style names, alliteration of first character of each word" )
+
+run :: Opts -> IO ()
+run (Opts w l s c u) = do
+    putStrLn $ generate w s
 
 main :: IO ()
-main = someFunc
+main = run =<< execParser opts
+    where
+        opts = info (options <**> helper)
+            ( fullDesc
+            <> progDesc "A utility to generate \"pet names\", consisting of a random combination of adverbs, an adjective and an animal name" )
